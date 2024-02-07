9a23a8294efb71e1784bada78020f3f1178e4e11
naasha13 <159261511+naasha13@users.noreply.github.com>
2024-02-07 18:31:52 +0600

    Add files via upload

---
+++ Lab Assignment-1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int main (){
+
+    double num1=124.01267, num2=0.00245, result;
+    result=num1+num2;
+    printf("the sum of num1 and num2 %lf",result);
+
+
+
+    float a= 1987, b=0.1;
+    result=a+b;
+
+    printf("the sum of a and b %f",result);
+
+
+    int c=10, d=8;
+    result=c+d;
+
+    printf("the sum of c and d %d",result);
+
+
+    int e=12;
+    float f=1.9;
+    result= e+f;
+
+    printf("the sum of e+f %f",result);
+
+
+
+
+    int g=9;
+    double h=3.023;
+    result= g+h;
+
+    printf("the sum of g+h %lf",result);
+
+
+
+
+    float i=4.1;
+    double j=9.0000001;
+    result=i+j;
+    printf("the sum of i+j %lf",result);
+
+
+    char k='A';
+    int l=9;
+    result=k+l;
+
+    printf("the sum of k+l %d", result);
+}
+
+
+
